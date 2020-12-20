79599b9f6cb8e8faa0681147149529d575e84d44
eduardo mancilla <eduardo.mancilla@upax.com.mx>
2020-12-20 15:55:16 -0600

    [UPDATE] framework compiled

---
+++ Carthage/Build/iOS/CarthageFrameworkTest.framework/Headers/CarthageFrameworkTest.h
@@ -0,0 +1,18 @@
+//
+//  CarthageFrameworkTest.h
+//  CarthageFrameworkTest
+//
+//  Created by eduardo mancilla on 20/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarthageFrameworkTest.
+FOUNDATION_EXPORT double CarthageFrameworkTestVersionNumber;
+
+//! Project version string for CarthageFrameworkTest.
+FOUNDATION_EXPORT const unsigned char CarthageFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageFrameworkTest/PublicHeader.h>
+
+
